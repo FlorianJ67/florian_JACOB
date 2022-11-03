359a17c8671f633b2e6f1e504e17ebae1754ec10
Florian JACOB <manciet@hotmail.fr>
2022-11-03 10:00:37 +0100

    commit surprise

--- AlgoPHP/Partie1/exo10.php
+++ AlgoPHP/Partie1/exo10.php
@@ -2,29 +2,6 @@
 
 <?php
 
-$age = 18;
-// true = femme | false = homme
-$femme = true;
+$price = 152;
 
-if ($femme == true){
-    if ($age >= 18 & $age <= 35){
-        echo "Age : .$age. <br>
-            Sexe: F<br>
-            La personne est imposable.";
-    } else {
-        echo "Age : .$age. <br>
-        Sexe: F<br>
-        La personne n'est pas imposable.";
-    } 
-} else {
-    if ( $age >= 20){
-        echo "Age : .$age. <br>
-            Sexe: H<br>
-            La personne est imposable.";
-    } else {
-        echo "Age : .$age. <br>
-        Sexe: H<br>
-        La personne n'est pas imposable.";
-    }
-}
 ?>
