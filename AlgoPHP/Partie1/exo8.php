bdf2ba7152fb58d53a0795734b890ea69118b548
Florian JACOB <manciet@hotmail.fr>
2022-11-03 09:47:35 +0100

    ajout d'exemple 2

--- AlgoPHP/Partie1/exo8.php
+++ AlgoPHP/Partie1/exo8.php
@@ -10,4 +10,11 @@ for ($i = 1; $i<=10; $i++) {
     echo "$i x $nb = $result<br>";
 }
 
+echo "<br>";
+$i = 1;
+while ($i <= 10){
+    $result = $i * $nb;
+    echo "$i x $nb = $result<br>";
+    $i++;
+}
 ?>
