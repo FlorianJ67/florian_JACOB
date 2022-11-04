e5766df18c814ff5eca13db8d1cff012b606c065
Florian JACOB <florian.jacob96@gmail.com>
2022-11-04 14:06:15 +0100

    exo 10

--- AlgoPHP/Partie1/exo10.php
+++ AlgoPHP/Partie1/exo10.php
@@ -1,7 +1,44 @@
-<h1>Exercice 9</h1>
+<h1>Exercice 10</h1>
 
 <?php
 
 $price = 152;
+$payment = 200;
+$change = $payment - $price;
+
+if ($change < 0){
+    echo "Transaction annuler; payement insuffisant";
+} else {
+    echo "Montant à payer : $price €<br>
+        Montant versé : $payment €<br>
+        Reste à payer : $change €<br>";
+} if ($change = 0) {
+
+} else {
+    $ten = 0;
+    $five = 0;
+    $two = 0;
+    $one = 0;
+    echo "*****************************<br>
+    Rendu de monaie :<br>";
+    while ($change >= 10) {
+        $ten++;
+        $change--;
+        echo "$ten billet de 10€ - ";
+    } while ($change >= 5) {
+        $five++;
+        $change--;
+        echo "$five billet de 5€ - ";
+    } while ($change >= 2) {
+        $two++;
+        $change--;
+        echo "$two pièce de 2€ - ";
+    } if ($change >= 1) {
+        $one++;
+        $change--;
+        echo "$one pièce de 1€";
+    }
+
+}
 
 ?>
