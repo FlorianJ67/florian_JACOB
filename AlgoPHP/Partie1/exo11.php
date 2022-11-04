10ab7f6682bd86c66835b5da4f2000bc650e9fb7
Florian JACOB <florian.jacob96@gmail.com>
2022-11-04 15:33:34 +0100

    exo 11 done

---
+++ AlgoPHP/Partie1/exo11.php
@@ -0,0 +1,8 @@
+<h1>Exercice 11</h1>
+
+<?php
+
+$voiture = array('Peugeot', 'Renault', 'BMW', 'Mercedes');
+echo ' - '.$voiture[0].'<br> - '.$voiture[1].'<br> - '.$voiture[2].'<br> - '.$voiture[3];
+
+?>
