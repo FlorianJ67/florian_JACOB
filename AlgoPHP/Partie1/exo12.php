1582fd4a835fc1168973bbdcf318fc103d314612
Florian JACOB <florian.jacob96@gmail.com>
2022-11-04 16:40:57 +0100

    1ère partie faite

---
+++ AlgoPHP/Partie1/exo12.php
@@ -0,0 +1,22 @@
+<h1>Exercice 12</h1>
+
+<?php
+
+$userT = [
+    "Mickaël" => "FRA",
+    "Virgile" => "ESP",
+    "Marie-Claire" => "ENG"
+];
+
+foreach ($userT as $user => $langue) {
+     if ($langue == "FRA") {
+        echo "Salut ";
+     } else if ($langue == "ESP") {
+        echo "Hola ";
+     } else if ($langue == "ENG") {
+        echo "Hello ";
+    }
+    echo $user. "<br>";
+ }
+
+?>
