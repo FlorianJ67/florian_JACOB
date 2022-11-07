f96145ce7c052b2077d90ed097f51947abee55da
Florian JACOB <florian.jacob96@gmail.com>
2022-11-07 10:32:29 +0100

    fin de pause

---
+++ AlgoPHP/Partie1/exo13.php
@@ -0,0 +1,22 @@
+<h1>Exercice 13</h1>
+
+<?php
+
+$table = [
+    10,
+    12,
+    8,
+    19,
+    3,
+    16,
+    11,
+    13,
+    9
+];
+
+
+
+$moyenne = ;
+
+echo "Sa moyenne générale est donc de :" $moyenne;
+?>
