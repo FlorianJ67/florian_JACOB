623e14dcfa7f1d91c7f791240a37226a3ee38b9b
Florian JACOB <florian.jacob96@gmail.com>
2022-11-08 10:55:42 +0100

    14 fini

--- AlgoPHP/Partie1/exo14.php
+++ AlgoPHP/Partie1/exo14.php
@@ -2,4 +2,11 @@
 
 <?php
 
+$birthDay=date_create("1985-01-17");
+$today=date_create("2018-05-21");
+$age=date_diff($birthDay,$today);
+
+echo "Age de la personne : " .$age->y. "ans " .$age->m. "mois " .$age->d. "jours";
+
+
 ?>
