1960f2d3de5f7c16505935f864902b61d55b6a17
Florian JACOB <florian.jacob96@gmail.com>
2022-11-10 15:23:58 +0100

    reglage date_diff + ajout set

--- AlgoPHP/Partie1/exo15.php
+++ AlgoPHP/Partie1/exo15.php
@@ -15,6 +15,7 @@ class Personne {
         $this->_birthday = $bday;
     }
 
+    //get
     public function getName(){
         return $this->_name;
     }
@@ -24,26 +25,45 @@ class Personne {
     public function getBirthDay(){
         return $this->_birthday;
     }
+    //set
+    public function setName($name){
+        $this->_name=$name;
+    }
+    public function setFirstName($name){
+        $this->_fname=$fname;
+    }
+    public function setBirthDay($name){
+        $this->_birthday=$bday;
+    }
+    //calcule de l'age
+    private function calcAge(){
+
+        $today=date_create();
+        $bd=$this->getBirthDay();
+        $bd=date_create($bd);
             
+        $age= date_diff($bd,$today);
+
+        return $age;
+    }
+
+    public function displayUser () {
+       
+        $age = $this->calcAge();
+    
+        echo $this->getFirstName() ." ". $this->getName() ." a " .$age->y. " ans<br>";
+    
+    }
+
 };
 
 //creation des utilisateur
 $p1 = new Personne("DUPONT","Michel","1980-02-19");
 $p2 = new Personne("DUCHEMIN","Alice","1985-01-17");
 
+$p1->setName("DUPOND");
 //fonction d'affichage utilisateur
-function displayuser ($user) {
-    $bd=$user->getBirthDay();
-    $today=date("Y-m-d");
-    echo $bd;
-    echo "<br>" .$today;
-    $age= date_diff($bd,$today);
 
-
-    echo $user->getFirstName() ." ". $user->getName() ." a " ./*$age->y.*/ " ans<br>";
-
-}
-
-displayuser($p1);
+$p1->displayUser();
 
 ?>
